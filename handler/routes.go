42316e63f8ce4077f4b59a640cc1abeed5a7ae43
yaestray <50524833+yaestray@users.noreply.github.com>
2024-05-12 04:32:13 +0300

    Update routes.go

--- handler/routes.go
+++ handler/routes.go
@@ -23,11 +23,11 @@ import (
 	"golang.zx2c4.com/wireguard/wgctrl"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 
-	"github.com/ngoduykhanh/wireguard-ui/emailer"
-	"github.com/ngoduykhanh/wireguard-ui/model"
-	"github.com/ngoduykhanh/wireguard-ui/store"
-	"github.com/ngoduykhanh/wireguard-ui/telegram"
-	"github.com/ngoduykhanh/wireguard-ui/util"
+	"github.com/yaestray/wireguard-ui/emailer"
+	"github.com/yaestray/wireguard-ui/model"
+	"github.com/yaestray/wireguard-ui/store"
+	"github.com/yaestray/wireguard-ui/telegram"
+	"github.com/yaestray/wireguard-ui/util"
 )
 
 var usernameRegexp = regexp.MustCompile("^\\w[\\w\\-.]*$")
