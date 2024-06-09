8a010c5a9a2c46a9b577656983abc33a7735e477
yaestray <swchyt@gmail.com>
2024-06-09 17:36:53 +0300

    Update helper.js

--- custom/js/helper.js
+++ custom/js/helper.js
@@ -64,7 +64,7 @@ function renderClientList(data) {
                                         data-clientname="${obj.Client.name}">Email</button>
                                 </div>
                                 ${telegramButton}
-								<button class="btn btn-outline-success btn-sm" onclick="sendNotification('${obj.username}', '${obj.expired_at}', '${obj.telegram_userid}')">Pay</button>
+								<button class="btn btn-outline-success btn-sm" onclick="sendNotification('${obj.Client.name}', '${obj.Client.ExpiredAt}', '${obj.Client.telegram_userid}')">Pay</button>
                                 <div class="btn-group">
                                     <button type="button" class="btn btn-outline-danger btn-sm">More</button>
                                     <button type="button" class="btn btn-outline-danger btn-sm dropdown-toggle dropdown-icon" 
